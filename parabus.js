c9c99818fa012d5fc36a0f0f76accd014766bd9a
Sstyles <sstyl001@somerset.colegia.org>
2024-02-13 12:22:52 -0500

    bus array seats

--- parabus.js
+++ parabus.js
@@ -7,6 +7,9 @@ console.log("Rows", seatRows);
 console.log("Cols", seatCols);
 console.log("Seats", seatCols * seatRows);
 
+var occuSeats = ['5','8','15','42'];
+var muteSeats = [];
+
 function password(){
   if (localStorage.getItem("safe").includes("true")) {
     document.getElementById("popup").style.display = "none";
@@ -28,8 +31,13 @@ while (seatCount <= (seatCols * seatRows)) {
   seat.classList.add("seat");
   seat.classList.add(`seat-${seatCount}`);
   seat.innerText = seatCount;
+  if (occuSeats.includes(String(seat.classList).match(/\d+/)[0])) {
+    console.log("Seat " + (String(seat.classList).match(/\d+/)[0]) + " is occupied");
+    seat.classList.add(`occuSeats`);
+  }
   seating.append(seat);
   seatCount++;
+
 }
 
 var selectedSeat;
@@ -46,13 +54,14 @@ $(".seat").click(function(){
 
 function seatDistance(){
   currentSeat = targetSeat;
+  muteSeats.push(((targetSeat[0]).className).match(/\d+/)[0]);
   var i = seatNumber;
-  while (i < (seatNumber+9)) {
-    // console.log(i);
+  while (i < (seatNumber+9) && i < seatCount-1) {
+    console.log(i);
     currentSeat =  $(currentSeat).next();
     currentSeat.addClass("orangeSeat");  
+    muteSeats.push(((currentSeat[0]).className).match(/\d+/)[0]);
     i++;
-    console.log(i);
   }    
   while (i < seatCount) {
     currentSeat =  $(currentSeat).next();
@@ -61,23 +70,28 @@ function seatDistance(){
   }    
   currentSeat = targetSeat;
   var i = seatNumber;
-  while (i > seatNumber-9) {
+  while (i > seatNumber-9 && i > 1) {
     currentSeat =  $(currentSeat).prev();
     currentSeat.addClass("orangeSeat");  
     i--;
-    console.log(i);
+    muteSeats.push(((currentSeat[0]).className).match(/\d+/)[0]);
   }     
   while (i > 0) {
     currentSeat =  $(currentSeat).prev();
     currentSeat.addClass("greenSeat");  
     i--;
-  }             
+  }            
+  muteSeats.sort(function(a, b) {
+    return a - b;
+  });
+  console.log(typeof muteSeats[3]); 
 }
 
 function wipeSeats() {
   $('.seat').each(function(i, obj) {
     if (String(this.classList).includes("redSeat")){
       this.classList.remove("redSeat");
+      muteSeats.length = 0;
     }
     if (String(this.classList).includes("orangeSeat")){
       this.classList.remove("orangeSeat");
